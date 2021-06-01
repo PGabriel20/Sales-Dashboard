29dc139053748320f72ea592c5e384abe329cd03
PGabriel20 <bielporreca@gmail.com>
2021-05-31 22:13:09 -0300

    feat: added side menu footer

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { FaChartBar, FaAddressBook } from "react-icons/fa";
+import { FaChartBar, FaAddressBook, FaCalendarAlt } from "react-icons/fa";
 
 import './styles.scss';
 
 const Aside: React.FC = () => {
   return (
     <aside className='sideMenu'>
+      <h2>Bem vindo(a)</h2>
      <nav>
        <a href="/"><div className='tag'/>Dashboard <FaChartBar /></a>
-       <a href=""><div className='tag'/>Cadastrar <FaAddressBook /></a>
+       <a href="/Produtos"><div className='tag'/>Cadastrar <FaAddressBook /></a>
      </nav>
+     <footer>
+       <FaCalendarAlt />
+        <strong>24/05/2021</strong>
+     </footer>
     </aside>
   );
 }
