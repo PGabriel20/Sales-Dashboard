31bb41966bc4653aa6a10f3744e8d06603d7715f
PGabriel20 <bielporreca@gmail.com>
2021-06-12 11:56:12 -0300

    feat: styling delete modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,52 @@
 import React from 'react';
+import { useContext } from 'react';
+import { FaRegTrashAlt, FaTimes } from "react-icons/fa";
+import { ModalContext } from '../../contexts/ModalContext';
+import api from '../../services/api';
 
 import './styles.scss'
 
 const Modal:React.FC = () => {
+
+  const {
+    setIsOpen, setDeleted,
+    type, id
+  } = useContext(ModalContext);
+
+  async function handleDelete(){
+    // since the modal can be used to delete both customers and sales
+    //we pass the property type to distinguish the user action
+
+    var url = '';
+
+    if(type==='sales'){
+      url = 'sale';
+    }
+    else{
+      url = 'costumer';
+    }
+    await api.delete(`${url}/${id}`).then((res)=>{
+      setDeleted(true);
+      setIsOpen(false);
+    }).catch(err=>{
+      alert(err)
+    })
+  }
+
   return(
     <div className='overlay'>
       <div className='container'>
-        <h1>Sou um Modal</h1>
+        <FaTimes />
+        <header>
+          <FaRegTrashAlt />
+          {}
+          <span>Are you sure you want to delete the {type==='sales'?'sale':'Customer'}?</span>
+          {type==='sales'?<p>This will affect the chart!</p>:undefined}
+        </header>
+        <section>
+          <button onClick={()=>{setIsOpen(false)}}>Cancel</button>
+          <button onClick={handleDelete}>Confirm</button>
+        </section>
       </div>
     </div>
   );
